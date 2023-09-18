a857b834346e7118a0ccc2a8ca2fcbd7feb7634f
kBlankii <k937137289@163.com>
2023-09-19 01:54:00 +0800

    fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Media;
+using System.Windows.Media; 
 using System.Threading;
 using System.Globalization;
+using System.Collections.Generic;
 //using System.Diagnostics;
 
 namespace NMS_PauseBeGone
@@ -19,18 +20,41 @@ namespace NMS_PauseBeGone
         [DllImport("user32.dll")]
         private static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
+        private Dictionary<string, string> lang_map = new Dictionary<string, string>();
+
+
         public MainWindow()
         {
             InitializeComponent();
             CultureInfo currentCulture = CultureInfo.CurrentCulture;
             Thread.CurrentThread.CurrentUICulture = currentCulture;
 
-            chkbox_active.Content = lang.Resource1.toolActivate;
-            window_main.Title = lang.Resource1.toolTitle;
-
+            //启动时钟
             aTimer = new System.Timers.Timer(500);
             aTimer.Elapsed += new System.Timers.ElapsedEventHandler(dt_Tick);
             aTimer.Enabled = true;
+
+            //初始化语言
+            string language = CultureInfo.CurrentCulture.Name;
+            if (language.StartsWith("zh"))
+            {
+                lang_map["chkbox_active"] = "使工具生效";
+                lang_map["window_main"] = "别tm再暂停了！";
+                lang_map["label_gameStatus"] = "游戏状态：";
+                lang_map["gameStatus_on"] = "运行中";
+                lang_map["gameStatus_off"] = "未找到";
+            } 
+            else
+            {
+                lang_map["chkbox_active"] = "Tool Activated";
+                lang_map["window_main"] = "PAUSE BE GONE";
+                lang_map["label_gameStatus"] = "Game Status: ";
+                lang_map["gameStatus_on"] = "Running";
+                lang_map["gameStatus_off"] = "Not Found";
+            }
+            chkbox_active.Content = lang_map["chkbox_active"];
+            window_main.Title = lang_map["window_main"];
+
         }
 
         void dt_Tick(object sender, EventArgs e)
@@ -41,7 +65,7 @@ namespace NMS_PauseBeGone
             {
                 if (hWnd != IntPtr.Zero)
                 { //找到窗口
-                    label_gameStatus.Content = lang.Resource1.gameStatus + lang.Resource1.gameRunning;
+                    label_gameStatus.Content = lang_map["label_gameStatus"] + lang_map["gameStatus_on"];
                     label_gameStatus.Foreground = new SolidColorBrush(Colors.Green);
                     if ((bool)chkbox_active.IsChecked)
                     {
@@ -50,9 +74,8 @@ namespace NMS_PauseBeGone
                 }
                 else
                 { //未找到窗口
-                    label_gameStatus.Content = lang.Resource1.gameStatus + lang.Resource1.gameNotFound;
+                    label_gameStatus.Content = lang_map["label_gameStatus"] + lang_map["gameStatus_off"];
                     label_gameStatus.Foreground = new SolidColorBrush(Colors.Maroon);
-
                 };
             });
         }
